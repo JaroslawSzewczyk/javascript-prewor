2b12877ed0de96bff0fce5d6298efd1d5fff89e5
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2019-11-05 20:59:51 +0000

    Change variable declaration

--- js/script.js
+++ js/script.js
@@ -1,63 +1,72 @@
-//Główna funkcja
-function playGame(playerInput) {
+{
+    let compWin = 0;
+    let playerWin = 0;
 
-    clearMessages()
+    //Główna funkcja
+    const playGame = function (playerInput) {
 
-    //Fukcja odpowiedzialna za wybór 
-    function getMoveName(argMoveId) {
-        if (argMoveId == 1) {
-            return 'kamień';
-        } else if (argMoveId == 2) {
-            return 'papier';
-        } else if (argMoveId == 3) {
-            return 'nożyce';
-        }
-        printMessage('Nie znam ruchu o id ' + argMoveId + '.');
-        return 'nieznany ruch';
-    }
+        const playerWins = document.querySelector(".wins");
+        const compWins = document.querySelector(".lose");
 
+        clearMessages();
 
-    //Funkcja wyświetla wynik gry
-    function displayResult(argComputerMove, argPlayerMove) {
-        if (argComputerMove == argPlayerMove) {
-            return 'remis';
-        } else if (argPlayerMove == 'nieznany ruch') {
+        //Fukcja odpowiedzialna za wybór 
+        const getMoveName = function (argMoveId) {
+            if (argMoveId == 1) {
+                return 'kamień';
+            } else if (argMoveId == 2) {
+                return 'papier';
+            } else if (argMoveId == 3) {
+                return 'nożyce';
+            }
+            printMessage('Nie znam ruchu o id ' + argMoveId + '.');
             return 'nieznany ruch';
-        } else if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
-            return 'Ty wygrywasz!';
-        } else {
-            return 'Wygrałem!!!'
         }
+
+
+        //Funkcja wyświetla wynik gry
+        const displayResult = function (argComputerMove, argPlayerMove) {
+            if (argComputerMove == argPlayerMove) {
+                return 'remis';
+            } else if (argPlayerMove == 'nieznany ruch') {
+                return 'nieznany ruch';
+            } else if ((argComputerMove == 'kamień' && argPlayerMove == 'papier') || (argComputerMove == 'papier' && argPlayerMove == 'nożyce') || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')) {
+                playerWins.textContent = "Wygrałeś: " + ++playerWin;
+                return 'Ty wygrywasz!';
+            } else {
+                compWins.textContent = "Wygrałem: " + ++compWin;
+                return 'Wygrałem!!!';
+            }
+        }
+
+        //Losowanie, przypisanie oraz wyświetlenie wyboru komputera  
+        const randomNumber = Math.floor(Math.random() * 3 + 1);
+        const computerMove = getMoveName(randomNumber);
+        printMessage('Mój ruch to: ' + computerMove);
+
+        //Wybranie, przypisanie oraz wyświetlenie wyboru gracza
+        const playerMove = getMoveName(playerInput);
+        printMessage('Twój ruch to: ' + playerMove);
+
+        //Wyświetlenie wyniku rozgrywki
+        printMessage(displayResult(computerMove, playerMove));
+
     }
 
-    //Losowanie, przypisanie oraz wyświetlenie wyboru komputera  
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
-    let computerMove = getMoveName(randomNumber);
-    printMessage('Mój ruch to: ' + computerMove);
 
-    //Wybranie, przypisanie oraz wyświetlenie wyboru gracza
-    let playerMove = getMoveName(playerInput);
-    printMessage('Twój ruch to: ' + playerMove);
+    const btnRock = document.getElementById('play-rock');
+    const btnPaper = document.getElementById('play-paper');
+    const btnScissors = document.getElementById('play-scissors');
 
+    btnRock.addEventListener('click', function () {
+        playGame(1);
+    });
 
-    //Wyświetlenie wyniku rozgrywki
-    printMessage(displayResult(computerMove, playerMove));
+    btnPaper.addEventListener('click', function () {
+        playGame(2);
+    });
 
+    btnScissors.addEventListener('click', function () {
+        playGame(3);
+    });
 }
-
-
-const btnRock = document.getElementById('play-rock');
-const btnPaper = document.getElementById('play-paper');
-const btnScissors = document.getElementById('play-scissors');
-
-btnRock.addEventListener('click', function () {
-    playGame(1);
-});
-
-btnPaper.addEventListener('click', function () {
-    playGame(2);
-});
-
-btnScissors.addEventListener('click', function () {
-    playGame(3);
-});
